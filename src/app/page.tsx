458f3e0a394caf5e321afd9d4564617331425e3b
amitnarw <71306477+amitnarw@users.noreply.github.com>
2024-07-10 16:37:16 +0530

    Everything present in main deleted

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import PdfCompo from "./components/PdfCompo";
-
-export default function Home() {
-  return (
-    <main className="flex justify-center items-center">
-      <PdfCompo />
-    </main>
-  );
-}
