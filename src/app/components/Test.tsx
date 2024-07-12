99ec73ca77e76920d2ca2f91a324759ce2d4c2a3
Amit <amitnarwal115@gmail.com>
2024-07-12 10:15:17 +0530

    Checking on mobile devices 12 June

--- src/app/components/Test.tsx
+++ src/app/components/Test.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-// import Html from "react-pdf-html";
 import {
   Page,
   Text,
@@ -9,15 +8,9 @@ import {
   StyleSheet,
   Font,
   PDFDownloadLink,
+  PDFViewer,
 } from "@react-pdf/renderer";
-import dynamic from "next/dynamic";
-//I added this imports to add suport to textLayer and anotations
-import "react-pdf/dist/Page/AnnotationLayer.css";
-import "react-pdf/dist/Page/TextLayer.css";
-import { isMobile } from "react-device-detect";
 import { FiDownload } from "react-icons/fi";
-
-console.log(isMobile, "mobile");
 
 Font.register({
   family: "OpenSans",
@@ -33,42 +26,44 @@ Font.register({
   ],
 });
 
-const PDFViewer = dynamic(
-  () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
-  {
-    ssr: false,
-    loading: () => <p>Loading...</p>,
-  }
-);
-
 export default function Home(props: any) {
   const [text, setText] = useState<any>();
+  const [loaded, setLoaded] = useState(false);
+
   useEffect(() => {
     setText(props.text);
   }, [props.text]);
 
+  useEffect(() => {
+    setLoaded(true);
+  });
+
   return (
     <div>
-      <PDFViewer className="h-screen w-full">
-        <MyPDFDocument text={text} />
-      </PDFViewer>
-      <PDFDownloadLink
-        document={<MyPDFDocument text={text} />}
-        fileName="example.pdf"
-      >
-        {({ blob, url, loading, error }) =>
-          loading ? (
-            <button className="rounded-xl px-5 py-2 bg-red-500/50 m-auto">
-              Loading document
-            </button>
-          ) : (
-            <button className="bg-[#1abc9c] hover:bg-[#16a085] p-6 rounded-lg flex items-center justify-center gap-2 text-white duration-300 sm:w-96 w-44 m-auto mt-20">
-              <FiDownload size={15} />
-              Download
-            </button>
-          )
-        }
-      </PDFDownloadLink>
+      {typeof window !== "undefined" && loaded && (
+        <>
+          <PDFViewer className="h-screen w-full">
+            <MyPDFDocument text={text} />
+          </PDFViewer>
+          <PDFDownloadLink
+            document={<MyPDFDocument text={text} />}
+            fileName="example.pdf"
+          >
+            {({ blob, url, loading, error }) =>
+              loading ? (
+                <button className="rounded-xl px-5 py-2 bg-red-500/50 m-auto">
+                  Loading document
+                </button>
+              ) : (
+                <button className="bg-[#1abc9c] hover:bg-[#16a085] p-6 rounded-lg flex items-center justify-center gap-2 text-white duration-300 sm:w-96 w-44 m-auto mt-20">
+                  <FiDownload size={15} />
+                  Download
+                </button>
+              )
+            }
+          </PDFDownloadLink>
+        </>
+      )}
     </div>
   );
 }
@@ -429,74 +424,3 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
 });
-
-const html = `<html>
-  <body>
-    <style>
-      .my-heading4 {
-        background: darkgreen;
-        color: white;
-      }
-      pre {
-        background-color: #eee;
-        padding: 10px;
-      }
-    </style>
-    <h1>Heading 1</h1>
-    <h2 style="background-color: pink">Heading 2</h2>
-    <h3>Heading 3</h3>
-    <h4 class="my-heading4">Heading 4</h4>
-    <p>
-      Paragraph with <strong>bold</strong>, <i>italic</i>, <u>underline</u>,
-      <s>strikethrough</s>,
-      <strong><u><s><i>and all of the above</i></s></u></strong>
-    </p>
-    <p>
-      Paragraph with image <img src="" /> and
-      <a href="http://google.com">link</a>
-    </p>
-    <hr />
-    <ul>
-      <li>Unordered item</li>
-      <li>Unordered item</li>
-    </ul>
-    <ol>
-      <li>Ordered item</li>
-      <li>Ordered item</li>
-    </ol>
-    <br /><br /><br /><br /><br />
-    Text outside of any tags
-    <table>
-      <thead>
-        <tr>
-          <th>Column 1</th>
-          <th>Column 2</th>
-          <th>Column 3</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Foo</td>
-          <td>Bar</td>
-          <td>Foobar</td>
-        </tr>
-        <tr>
-          <td colspan="2">Foo</td>
-          <td>Bar</td>
-        </tr>
-        <tr>
-          <td>Some longer thing</td>
-          <td>Even more content than before!</td>
-          <td>Even more content than before!</td>
-        </tr>
-      </tbody>
-    </table>
-    <div style="width: 200px; height: 200px; background: pink"></div>
-    <pre>
-function myCode() {
-  const foo = 'bar';
-}
-</pre>
-  </body>
-</html>
-`;
